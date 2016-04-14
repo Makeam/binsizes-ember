6559bb08b7939facc4041c3892c062b0c437e71b
Maslakov Denis <miel.design@gmail.com>
2016-04-14 17:27:52 +0300

    Config Ember App

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  Rails.application.routes.draw do
+    mount_ember_app :frontend, to: "/"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
